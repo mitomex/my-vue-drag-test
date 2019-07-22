ae113361cace4189f519573160a6d0f3c6a2b78a
Yusuke Mitome <info@mitomex.jp>
2019-07-22 15:33:43 -0700

    add dragula

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Drag />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Drag from "./components/Drag";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Drag
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "../node_modules/dragula/dist/dragula.min.css";
 </style>
---
+++ src/components/Drag.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <ul class="list" ref="list">
+      <li class="item">item01</li>
+      <li class="item">item02</li>
+      <li class="item">item03</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import dragula from "dragula";
+export default {
+  mounted() {
+    dragula([this.$refs.list]);
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.list {
+  padding: 0;
+}
+.item {
+  background-color: rgba(#000, 0.3);
+  cursor: grab;
+  list-style: none;
+  margin: 10px;
+  padding: 10px;
+}
+</style>
